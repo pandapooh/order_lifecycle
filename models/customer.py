f7a7f6b36cb5dc41458e43943946a7475fef45c0
pandapooh <medussin@gmail.com>
2024-10-20 13:13:47 -0600

    cambio de nombre de habitaciones

--- models/customer.py
+++ models/customer.py
@@ -3,8 +3,6 @@ import os
 from datetime import datetime
 from os.path import join, dirname
 from dotenv import load_dotenv
-from models.dynamodb import Dynamo
-from botocore.exceptions import ClientError
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
